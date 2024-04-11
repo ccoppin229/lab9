24a2e489a671f300d325d30589515aad99c0bd82
kmont1 <kmontero1@ufl.edu>
2024-04-11 15:02:06 -0400

    Added decode function and completed option 3

--- cec_Lab9.py
+++ cec_Lab9.py
@@ -8,6 +8,14 @@ def encode(password):
             char=char%3
         encodedpassword+=str(char)
     return encodedpassword
+
+def decode(password): # Decode Password Function - Added by Kevin Montero
+    decoded_pass = "" # Initializes empty string to store decoded digits
+    for digit in password: # Iterates through each digit in encoded password and shifts each number down by 3
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_pass += decoded_digit
+    return decoded_pass # Returns Decoded Password
+
 def main():
     while True:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
@@ -17,7 +25,9 @@ def main():
             encodedpassword=encode(password)
             print("Your password has been encoded and stored!")
         elif option==2:
-            pass
+            password=input("Enter password to decode: ")
+            password=decode(password)
+            print(f"Your decoded password is {password}")
         elif option==3:
             break
 
